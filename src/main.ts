823c375cb7b6431ea47d829a773243f647b27574
Carlos Daniel Martins de Almeida <carlosdaniiel0711@hotmail.com>
2021-07-04 15:07:23 -0300

    Add OpenAPI docs (Swagger)

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,22 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { GlobalErrorHandler } from '@shared/middlewares/global-error-handler.middleware';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalFilters(new GlobalErrorHandler());
   app.useGlobalPipes(new ValidationPipe());
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('PetShop API')
+    .setDescription('API desenvolvida com NestJs no curso 7180 do balta.io')
+    .setVersion('1.0')
+    .build();
+  const swagger = SwaggerModule.createDocument(app, swaggerConfig);
+
+  SwaggerModule.setup('docs', app, swagger);
 
   await app.listen(3000);
 }
